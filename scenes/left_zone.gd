47226b85a59294c4fe61c8d322aba5ca618a1457
krishnabm <krishnabmahadevan@gmail.com>
2025-09-17 16:40:50 +0530

    Touchscreen fixes

--- scenes/left_zone.gd
+++ scenes/left_zone.gd
@@ -1,12 +1,20 @@
 extends Control
 
-var pressed := false
 var action_name := "ui_select"
+var isTouch := false
+
+func _ready():
+	if OS.has_feature("web_android") or OS.has_feature("web_ios") or OS.has_feature("android") or OS.has_feature("ios"):
+		isTouch = true
 
 func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_LEFT:
-		Input.action_press(action_name) if event.pressed else Input.action_release(action_name)
-	elif event is InputEventScreenTouch:
-		Input.action_press(action_name) if event.pressed else Input.action_release(action_name)
+	if isTouch:
+		if event is InputEventScreenTouch:
+			Input.action_press(action_name) if event.pressed else Input.action_release(action_name)
+		else:
+			Input.action_release(action_name)
 	else:
-		Input.action_release(action_name)
+		if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_LEFT:
+			Input.action_press(action_name) if event.pressed else Input.action_release(action_name)
+		else:
+			Input.action_release(action_name)
